76b7be5e754bda773eb2cd60ff00fcdbfb1e4d10
hieugit123 <131244762+hieugit123@users.noreply.github.com>
2023-06-27 21:04:49 +0700

    tạo các tầng

---
+++ src/main/java/common/Bean.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package common;
+
+/**
+ *
+ * @author mxuan
+ */
+public class Bean {
+    public static String msg;
+    public static boolean _isMsgShow;
+}
